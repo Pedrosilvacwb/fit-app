ecff67411eb9022a666e36e36764b9e786662b3b
Pedro Silva <pedrosilvacwb@gmail.com>
2022-10-15 15:57:05 -0300

    add: add feat render exercises

--- src/components/Exercises.jsx
+++ src/components/Exercises.jsx
@@ -1,7 +1,79 @@
 import React from 'react';
+import ExerciseCard from './ExerciseCard';
+import { Pagination } from '@mui/material/';
+import { Box, Stack, Typography } from '@mui/material';
 
-const Exercises = () => {
-  return <div>Exercises</div>;
+import { exerciseOptions, fetchData } from '../utils/fetchData';
+
+const Exercises = ({ exercises, setExercises, bodyPart }) => {
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const exercisesPerPage = 9;
+  const indexOfLastExercise = currentPage * exercisesPerPage;
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage;
+  const currentExercise = exercises.slice(
+    indexOfFirstExercise,
+    indexOfLastExercise,
+  );
+
+  const paginate = (event, value) => {
+    setCurrentPage(value);
+    window.scrollTo({ top: 1800, behavior: 'smooth' });
+  };
+
+  React.useEffect(() => {
+    const fetchExerciseData = async () => {
+      let exerciseData = [];
+
+      if (bodyPart === 'all') {
+        exerciseData = await fetchData(
+          'https://exercisedb.p.rapidapi.com/exercises',
+          exerciseOptions,
+        );
+      } else {
+        exerciseData = await fetchData(
+          `https://exercisedb.p.rapidapi.com/exercises/bodyPart/${bodyPart}`,
+          exerciseOptions,
+        );
+      }
+
+      setExercises(exerciseData);
+    };
+
+    fetchExerciseData();
+  }, [bodyPart]);
+
+  return (
+    <Box id="exercises" sx={{ mt: { lg: '110px' } }} mt="50px" p="20px">
+      <Typography variant="h3" mb="46px">
+        Resultados{' '}
+      </Typography>
+
+      <Stack
+        direction="row"
+        sx={{ gap: { lg: '110px', xs: '50px' } }}
+        flexWrap="wrap"
+        justifyContent="center"
+      >
+        {currentExercise.map((ex, index) => {
+          return <ExerciseCard key={index} exercise={ex} />;
+        })}
+      </Stack>
+
+      <Stack mt="100px" alignItems="center">
+        {exercises.length > 9 && (
+          <Pagination
+            color="standard"
+            shape="rounded"
+            defaultPage={1}
+            count={Math.ceil(exercises.length / exercisesPerPage)}
+            page={currentPage}
+            onChange={paginate}
+            size="large"
+          />
+        )}
+      </Stack>
+    </Box>
+  );
 };
 
 export default Exercises;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,7 @@ const Home = () => {
       <Exercises
         setExercises={setExercises}
         bodyPart={bodyPart}
-        setBodyPart={setBodyPart}
+        exercises={exercises}
       />
     </Box>
   );
